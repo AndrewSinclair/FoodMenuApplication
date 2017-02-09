233f57f9612ae3ba0f90da44e992a3b4280f4b2a
Andrew <andrew.george.sinclair@gmail.com>
2017-02-09 12:40:25 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# food menu application
+
+This is the api component for our food menu app.
+
+## Prerequisites
+
+You will need [Leiningen][] 2.0.0 or above installed.
+
+[leiningen]: https://github.com/technomancy/leiningen
+
+## Running
+
+To start a web server for the application, run:
+
+    lein ring server
+
+
+And connect to `http://localhost:3000`
+
+## License
+
+Copyright © 2017
---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject food-app "0.1.0-SNAPSHOT"
+  :description "Food menu application"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler food-app.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
