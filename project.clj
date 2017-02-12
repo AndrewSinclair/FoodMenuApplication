385f55b33373463560c77595a0606f09d689b586
AndrewSinclair <andrew.george.sinclair@gmail.com>
2017-02-12 16:41:33 -0600

    Add Json support and filewatching

--- project.clj
+++ project.clj
@@ -5,12 +5,15 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
+                 [ring/ring-json "0.4.0"]
                  [org.clojure/java.jdbc "0.7.0-alpha1"]
                  [postgresql "9.1-901.jdbc4"]
                  [me.raynes/conch "0.8.0"]]
   :plugins [[lein-ring "0.9.7"]
             [lein-exec "0.3.1"]]
-  :ring {:handler food-app.handler/app}
+  :ring {:handler food-app.handler/app
+         :auto-relaod? true
+         :reload-paths ["src"]}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
--- src/food_app/handler.clj
+++ src/food_app/handler.clj
@@ -3,17 +3,21 @@
             [clojure.string :as string]
             [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.util.response :refer [response]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
 (defroutes app-routes
   (GET "/" []
     "Hello World")
 
   (GET "/menus" []
-    (string/join \newline (db/get-menu-titles)))
+    (db/get-menu-titles))
   
   (route/not-found "Not Found"))
 
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+    wrap-json-response
+    (wrap-defaults api-defaults)))
