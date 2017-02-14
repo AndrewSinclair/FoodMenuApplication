4671e18c925cb47241d1c08383d9c2c12548d9f1
AndrewSinclair <andrew.george.sinclair@gmail.com>
2017-02-13 19:45:43 -0600

    Add user table with test data

--- db/00_schema.sql
+++ db/00_schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE menu (
     body varchar(255)
 );
 ALTER SEQUENCE menu_id_seq OWNED BY menu.id;
+
+DROP TABLE IF exists user_account;
+CREATE SEQUENCE user_id_seq;
+CREATE TABLE user_account (
+    id smallint NOT NULL DEFAULT nextval('user_id_seq') PRIMARY KEY,
+    username varchar(255),
+    password varchar(255)
+);
+ALTER SEQUENCE user_id_seq OWNED BY user_account.id;
--- db/01_import-test-data.sql
+++ db/01_import-test-data.sql
@@ -1,2 +1,5 @@
 INSERT INTO menu(title, body) VALUES ('Cake', 'It''s very good');
 INSERT INTO menu(title, body) VALUES ('Pie',  'It''s very delicious');
+INSERT INTO user_account(username, password) VALUES ('andrew', 'qwerty');
+INSERT INTO user_account(username, password) VALUES ('george', 'asdf');
+
